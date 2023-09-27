abb45f8b950e7c67c8885bf4c7822b5bedef46c7
MeGilangR <megilangr1@gmail.com>
2023-09-27 19:55:48 +0700

    Logo Nama

--- resources/views/backend/layouts/master.blade.php
+++ resources/views/backend/layouts/master.blade.php
@@ -36,7 +36,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4 sidebar-no-expand">
     <a href="{{ route('backend.main') }}" class="brand-link">
       <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light ml-2">Base</span>
+      <span class="brand-text font-weight-light ml-2">{{ env('APP_NAME') ?? 'Base' }}</span>
     </a>
 
     <div class="sidebar">
